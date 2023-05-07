325139d8188f1e32dd24e130047464ad55b061f3
Roxi Zhauk <hiro@Hiros-MBP.local>
2023-05-07 13:08:59 +0900

    add

--- app/api/revalidate-path/route.ts
+++ app/api/revalidate-path/route.ts
@@ -2,11 +2,12 @@ import { NextRequest, NextResponse } from "next/server";
 import { revalidatePath } from "next/cache";
 
 export async function GET(request: NextRequest) {
+  const path = request.nextUrl.searchParams.get("path") || "/";
   try {
-    const path = request.nextUrl.searchParams.get("path") || "/";
     revalidatePath(path);
     return NextResponse.json({ revalidated: true });
   } catch (err) {
-    return NextResponse.json({ message: "Error revalidatin" });
+    console.log(err);
+    return NextResponse.json({ message: "Error revalidation", path });
   }
 }
