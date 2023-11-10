8bff9276b9cec5356aceaba3456ac5cd876e56ae
Roxi Zhauk <roxizhauk@gmail.com>
2023-11-10 10:01:45 +0900

    fixed

--- app/api/revalidate-path/route.ts
+++ app/api/revalidate-path/route.ts
@@ -1,17 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import { revalidatePath } from "next/cache";
+import { getDate } from "@/lib";
 
 export async function GET(req: NextRequest) {
   const { searchParams } = req.nextUrl;
   const path = searchParams.get("path") || "/";
-  const result = { path, revalidated: true };
-
-  try {
-    revalidatePath(path);
-  } catch (err) {
-    result.revalidated = false;
-    console.log(err);
-  } finally {
-    return NextResponse.json(result);
-  }
+  revalidatePath(path);
+  return NextResponse.json({ revalidated: true, path, datetime: getDate() });
 }
